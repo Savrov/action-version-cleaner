d5a990a9e088a08c201c0bfa5b4ebb85a1418e44
savrov <savrov.code@gmail.com>
2023-09-10 17:19:37 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
+    application
+}
+
+application {
+    mainClass.set("system.MainKt")
 }
 
 group = "team.credible"
