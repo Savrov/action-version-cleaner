595cd477854daf0235fce98e9fe3b92a3a59d219
savrov <savrov.code@gmail.com>
2023-09-10 18:15:36 +0200

    init

--- src/main/kotlin/domain/LoadPackagesByRepositoryUseCase.kt
+++ src/main/kotlin/domain/LoadPackagesByRepositoryUseCase.kt
@@ -4,16 +4,20 @@ import model.Package
 
 internal class LoadPackagesByRepositoryUseCase(
     private val packageRepository: PackageRepository,
-): SuspendUseCase<LoadPackagesByRepositoryUseCase.Params, Collection<Package>> {
+) : SuspendUseCase<LoadPackagesByRepositoryUseCase.Params, Collection<Package>> {
 
     override suspend fun invoke(input: Params): Result<Collection<Package>> {
         return packageRepository.loadPackages(
             organization = input.organization,
             packageType = input.packageType
-        ).map { list ->
+        ).also {
+            println("list size BEFORE=${it.getOrNull()?.count()}")
+        }.map { list ->
             list.filter { item ->
                 item.repository?.name == input.repository
             }
+        }.also {
+            println("list size AFTER=${it.getOrNull()?.count()}")
         }
     }
 
